01bdda1bdf5f4a5b92ab49b9d1f4bc52babbc617
draco13th <fgvedxfedtfc@hotmail.com>
2015-08-16 11:12:35 -0500

    updated with money

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var code = 0;
 var interns = 0;
 //Programs Variable
 var programs = 0;
+//Money Variable
+var money = 0;
+
 //Generate Code Function
 function createCode(number){
 	code = code + number;
@@ -20,14 +23,25 @@ function hireIntern(){
     };
     var nextCost = Math.floor(10 * Math.pow(1.2,interns));       	
     document.getElementById('internCost').innerHTML = nextCost;
+	document.getElementById('codeps').innerHTML = interns;
 };
 //Compile Program Function
 function compileProgram(){
 	if(code >= 50) {
 		code = code - 50;
-		programs = programs + 1
+		programs = programs + 1;
 		document.getElementById('code').innerHTML = code;
 		document.getElementById('programs').innerHTML = programs;
+	};
+};
+//Sell Program Function
+function sellprogram(){
+	var programvalue = Math.floor(10 * Math.pow(1.2,programs));
+	if(programs >= 1) {
+		programs = programs - 1;
+		money = money + programvalue;
+		document.getElementById('programs').innerHTML = programs;
+		document.getElementById('money_').innerHTML = money;
 	};
 };
 //Save Game function
@@ -36,8 +50,11 @@ function saveGame(){
 		code: code,
 		interns: interns,
 		programs: programs,
-	}
+		money: money,
+	};
 	localStorage.setItem("save",JSON.stringify(save));
+	alert("Game Saved");
+	
 };
 //Hard Reset Function
 function deleteSave(){
@@ -53,6 +70,7 @@ window.setInterval(function(){
 	document.getElementById('interns').innerHTML = interns;			//Update interns
     document.getElementById('code').innerHTML = code;				//Update code
 	document.getElementById('programs').innerHTML = programs;		//Update programs
+	document.getElementById('money_').innerHTML = money;			//Update money
 	var internCost = Math.floor(10 * Math.pow(1.2,interns));		//Keep track of the intern cost
 	document.getElementById('internCost').innerHTML = internCost;	//Update intern cost
 	var codeps = interns;											//Keep track of LoC per Second
